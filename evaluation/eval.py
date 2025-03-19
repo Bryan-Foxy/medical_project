9ba878da8a4e1feb00fd221dac5a4bc42ffe0854
Bryan-Foxy <armandbryan572@icloud.com>
2025-03-19 20:29:22 +0100

    eval script begin

---
+++ evaluation/eval.py
@@ -0,0 +1,13 @@
+"""
+Author:     FOZAME ENDEZOUMOU Armand Bryan 
+
+This code is for the evaluation for our soliution, 
+we will compare the original VHS to the predicted one.
+Also compare the diameters.
+
+We will perform MAE and MSE.
+"""
+
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
